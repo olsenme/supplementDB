fbce37ceb94f2b01332dbb5fcb9f8827f73b2661
Meagan <olsenme@oregonstate.edu>
2019-07-12 21:50:49 -0700

    added edit button/fixed security vulnerabilities

--- public/index.js
+++ public/index.js
@@ -5,14 +5,20 @@ function createProductCard(name, url, rating, quantity, price, description)
     var productCardSection = document.createElement('section');
     productCardSection.classList.add('product-card');
 
+    var textContainer = document.createElement('div');
+    textContainer.classList.add('center');
+    productCardSection.appendchild(textContainer);
+
     var nameContainerDiv = document.createElement('div');
     nameContainerDiv.classList.add('name-container');
     nameContainerDiv.textContent = name;
-    productCardSection.appendchild(nameContainerDiv);
+    textContainer.appendchild(nameContainerDiv);
+  //  productCardSection.appendchild(nameContainerDiv);
 
     var imgContainerDiv = document.createElement('div');
     imgContainerDiv.classList.add('img-container');
-    productCardSection.appendchild(imgContainerDiv);
+    textContainer.appendchild(imgContainerDiv);
+    //productCardSection.appendchild(imgContainerDiv);
 
     var img = document.createElement('img');
     img.classList.add('product-photo-img');
@@ -22,31 +28,37 @@ function createProductCard(name, url, rating, quantity, price, description)
     var ratingContainerDiv = document.createElement('div');
     nameContainerDiv.classList.add('rating-container');
     nameContainerDiv.textContent = rating;
-    productCardSection.appendchild(ratingContainerDiv);
+    textContainer.appendchild(ratingContainerDiv);
+    //productCardSection.appendchild(ratingContainerDiv);
 
     var quantityContainerDiv = document.createElement('div');
     nameContainerDiv.classList.add('quantity-container');
     nameContainerDiv.textContent = quantity;
-    productCardSection.appendchild(quantityContainerDiv);
+    textContainer.appendchild(quantityContainerDiv);
+    //productCardSection.appendchild(quantityContainerDiv);
 
     var priceContainerDiv = document.createElement('div');
     nameContainerDiv.classList.add('price-container');
     nameContainerDiv.textContent = price;
-    productCardSection.appendchild(priceContainerDiv);
-
-    var priceContainerDiv = document.createElement('div');
-    nameContainerDiv.classList.add('price-container');
-    nameContainerDiv.textContent = price;
-    productCardSection.appendchild(priceContainerDiv);
+    textContainer.appendchild(priceContainerDiv);
+  //  productCardSection.appendchild(priceContainerDiv);
 
     var descriptionContainerDiv = document.createElement('div');
     nameContainerDiv.classList.add('description-container');
-    productCardSection.appendchild(descriptionContainerDiv);
+    //productCardSection.appendchild(descriptionContainerDiv);
+    textContainer.appendchild(descriptionContainerDiv);
 
     var p = document.createElement('p');
     p.classList.add('product-description');
     p.textContent = description;
     descriptionContainerDiv.appendchild(p);
+
+    var button = document.createElement('button');
+    button.classList.setAttribute("id","delete-product-button");
+    var icon = document.createElement('i');
+    icon.classList.add('"fas fa-pencil-alt"');
+    button.appendchild(icon);
+    productCardSection.appendchild(button);
 
     return productCardSection;
 }
--- public/productCardTemplate.js
+++ public/productCardTemplate.js
@@ -3,7 +3,7 @@
 templates['productCard'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "<section class=\"product-card\">\r\n    <div class=\"name-container\">\r\n        "
+  return "<section class=\"product-card\">\r\n  <div class = \"center\">\r\n    <div class=\"name-container\">\r\n        "
     + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
     + "\r\n    </div>\r\n    <div class=\"img-container\">\r\n        <img class=\"product-photo-img\" src="
     + alias4(((helper = (helper = helpers.url || (depth0 != null ? depth0.url : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"url","hash":{},"data":data}) : helper)))
@@ -15,6 +15,6 @@ templates['productCard'] = template({"compiler":[7,">= 4.0.0"],"main":function(c
     + alias4(((helper = (helper = helpers.price || (depth0 != null ? depth0.price : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"price","hash":{},"data":data}) : helper)))
     + "\r\n    </div>\r\n    <div class=\"description-container\">\r\n        <p class=\"product-description\">\r\n        "
     + alias4(((helper = (helper = helpers.description || (depth0 != null ? depth0.description : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"description","hash":{},"data":data}) : helper)))
-    + "\r\n        </p>\r\n    </div>\r\n</section>\r\n";
+    + "\r\n        </p>\r\n    </div>\r\n  </div>\r\n    <button type=\"button\" id=\"edit-product-button\">\r\n      <i class=\"fas fa-pencil-alt\"></i>\r\n    </button>\r\n</section>\r\n";
 },"useData":true});
 })();
